3f118242e22b2ad494a8689a03eeec6d4ae4fd27
Edwin de Jong <info@edwindejong.net>
2018-04-18 14:02:59 +0200

    Table for actual weather info. Made weather warnings a bit more organized.

--- laravel-yr-api/resources/views/forecast/show.blade.php
+++ laravel-yr-api/resources/views/forecast/show.blade.php
@@ -5,18 +5,51 @@ Country : {{$forecast['location']['country']}}<br>
 Location: {{$forecast['location']['name']}}<br>
 <br>
 
-Today the weather will be like:<br>
+Today's Weather Warnings:<br>
 
 @foreach($forecast['forecast']['text']['location']['time'] as $time)
-Title: {{$time['title']}} <br>
-Detailed: {!!$time['body']!!}<br>
+    <p>
+        from: {{$time['@from']}}<br> {{--Should probably make this display in a much nicer format of time--}}
+        to: {{$time['@to']}}<br>
 
-from: {{$time['@from']}}<br>
-to: {{$time['@to']}}<br>
+        Title: {{$time['title']}} <br>
+        Details:<br> {!!$time['body']!!}<br>
+        <br>
+    </p>
+    <hr>
 @endforeach
 
-Yr demands these credits to be displayed, so here we go!<br>
+As for the weather itsself:<br>
+<br>
+<table border="1px">
+    <thead>
+    <tr>
+    <td>from</td>
+    <td>to</td>
+    <td>temperature</td>
+    <td>wind dir</td>
+    <td>wind speed</td>
+    <td>pressure</td>
+    </tr>
+    </thead>
+    <tbody>
+@foreach($forecast['forecast']['tabular']['time'] as $time)
 
+        <tr>
+            <td>{{$time['@from']}}</td>
+            <td>{{$time['@to']}}</td>
+            <td>{{$time['temperature']['@value']}}</td>
+            <td>{{$time['windDirection']['@name']}}</td>
+            <td>{{$time['windSpeed']['@name']}}</td>
+            <td>{{$time['pressure']['@value']}}</td>
+        </tr>
+@endforeach
+    </tbody>
+</table>
+
+<br>
+Yr demands these credits to be displayed, so here we go!<br>
+<br>
 <p>
     {{$forecast['credit']['link']['@text']}}
     <br>
